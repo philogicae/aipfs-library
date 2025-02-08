c315f47e6df5ec6ecab8afd0f687f8f5ae47344b
adrienvfd <vonfeld@gmail.com>
2025-02-09 00:44:36 +0200

    update loading component

--- dapp/components/frames/Loading.tsx
+++ dapp/components/frames/Loading.tsx
@@ -1,8 +1,7 @@
-
 export default function Loading() {
 	return (
-		<div className="flex flex-col items-center justify-center w-full h-full">
-			<span>Loading...</span>
+		<div className="absolute flex flex-col items-center justify-center w-full h-full">
+			<div className="h-12 w-12 animate-spin rounded-full border-3 border-solid border-current border-r-transparent motion-reduce:animate-[spin_1.5s_linear_infinite]"/>
 		</div>
 	)
 }
