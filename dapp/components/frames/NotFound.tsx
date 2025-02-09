d055812e1cd2fe8c84f61d086e050454d8e84968
philogicae <38438271+philogicae@users.noreply.github.com>
2025-02-09 04:52:34 +0200

    Change window label for small screens

--- dapp/components/frames/NotFound.tsx
+++ dapp/components/frames/NotFound.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
 	const router = useRouter();
 	return (
 		<PageWrapper>
-			<TerminalFrame subTitle="lost in an unknown dimension">
+			<TerminalFrame subTitle="lost signal">
 				<div className="flex flex-col items-center justify-center w-full h-full gap-2 pb-8">
 					<ExclamationCircleIcon className="w-24 h-24 text-red-500" />
 					<div className="text-4xl font-bold text-gray-200 leading-relaxed">
